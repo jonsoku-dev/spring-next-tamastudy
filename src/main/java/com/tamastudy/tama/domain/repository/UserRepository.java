e22fb7ea208d1a0bff3c9a6578b2b9bd859afe24
jonsoku2 <whdtjr2792@gmail.com>
2020-12-27 07:44:07 +0900

    Add user repository

---
+++ src/main/java/com/tamastudy/tama/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tamastudy.tama.domain.repository;
+
+import com.tamastudy.tama.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByUsername(String username);
+    User findByEmail(String email);
+}
