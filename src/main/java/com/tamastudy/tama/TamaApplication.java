09596412902388d67b20a31edf4ad20bd050051a
jonsoku2 <whdtjr2792@gmail.com>
2020-12-27 07:34:54 +0900

    init project

---
+++ src/main/java/com/tamastudy/tama/TamaApplication.java
@@ -0,0 +1,13 @@
+package com.tamastudy.tama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TamaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tamastudy/tama/TamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamastudy.tama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
